3f27b02902ea0cfa359c5bf6d0ef14bcd878d123
Eternal-Snowstorm <3512546747@qq.com>
2026-01-17 11:41:21 +0100

    先把下界窒息缓缓

--- src/main/java/top/nebula/cmi/event/PlayerDrownInNether.java
+++ src/main/java/top/nebula/cmi/event/PlayerDrownInNether.java
@@ -15,8 +15,8 @@ public class PlayerDrownInNether {
 			return;
 		}
 		if (pPlayer.level().dimension() == Level.NETHER) {
-			event.setCanBreathe(false);
-			event.setCanRefillAir(false);
+			// event.setCanBreathe(false);
+			// event.setCanRefillAir(false);
 		}
 	}
 }
