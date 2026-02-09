33787f9dc34a1a5aff0a2c4a31c9fb8718d42389
Eternal-Snowstorm <3512546747@qq.com>
2026-02-09 15:37:46 +0100

    修复一项配方id问题

--- src/main/java/top/nebula/cmi/compat/kubejs/ModKubeJSPlugin.java
+++ src/main/java/top/nebula/cmi/compat/kubejs/ModKubeJSPlugin.java
@@ -5,7 +5,7 @@ import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
 import dev.latvian.mods.kubejs.script.BindingsEvent;
 import top.nebula.cmi.Cmi;
 import top.nebula.cmi.compat.kubejs.recipe.AcceleratorSchema;
-import top.nebula.cmi.compat.kubejs.recipe.BeltGrinderSchema;
+import top.nebula.cmi.compat.kubejs.recipe.GrindingSchema;
 import top.nebula.cmi.utils.CmiLang;
 
 public class ModKubeJSPlugin extends KubeJSPlugin {
@@ -13,7 +13,7 @@ public class ModKubeJSPlugin extends KubeJSPlugin {
 	public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
 		event.namespace(Cmi.MODID)
 				.register("accelerator", AcceleratorSchema.SCHEMA)
-				.register("belt_grinder", BeltGrinderSchema.SCHEMA);
+				.register("grinding", GrindingSchema.SCHEMA);
 	}
 
 	public void registerBindings(BindingsEvent event) {
