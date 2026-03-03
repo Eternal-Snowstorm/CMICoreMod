b7a602ecb97fb353d74923de5c4e5bfffb89f60c
Eternal-Snowstorm <3512546747@qq.com>
2026-03-03 13:58:18 +0800

    Update CoilItem.java

--- src/main/java/dev/celestiacraft/cmi/common/item/mechanism/CoilItem.java
+++ src/main/java/dev/celestiacraft/cmi/common/item/mechanism/CoilItem.java
@@ -1,36 +1,13 @@
 package dev.celestiacraft.cmi.common.item.mechanism;
 
-import com.simibubi.create.foundation.item.CustomArmPoseItem;
 import dev.celestiacraft.cmi.Cmi;
-import dev.celestiacraft.cmi.common.item.equipments.railgun.BuiltinRailgunAmmoTypes;
-import dev.celestiacraft.cmi.common.item.equipments.railgun.RailGunAmmoType;
-import dev.celestiacraft.cmi.common.item.equipments.railgun.RailgunAmmoProvider;
-import dev.celestiacraft.cmi.common.item.equipments.railgun.RailgunPacket;
-import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ProjectileWeaponItem;
+import dev.celestiacraft.cmi.common.item.MechanismItem;
 import net.minecraftforge.fml.common.Mod;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.function.Predicate;
 
 @Mod.EventBusSubscriber(modid = Cmi.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class CoilItem extends ProjectileWeaponItem implements CustomArmPoseItem {
+public class CoilItem extends MechanismItem {
 
-	@Override
-	public HumanoidModel.@Nullable ArmPose getArmPose(ItemStack stack, AbstractClientPlayer player, InteractionHand hand) {
-		return null;
-	}
-
-	@Override
-	public Predicate<ItemStack> getAllSupportedProjectiles() {
-		return null;
-	}
-
-	@Override
-	public int getDefaultProjectileRange() {
-		return 0;
+	public CoilItem(Properties properties) {
+		super(properties);
 	}
 }
