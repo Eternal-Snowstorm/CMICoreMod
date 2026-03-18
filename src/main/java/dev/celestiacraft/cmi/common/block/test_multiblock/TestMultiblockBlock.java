efa8dd8e1e3c43f2a07e75c2a8515560e1edb9a8
Qi-Month <dy757410@163.com>
2026-03-18 21:05:37 +0800

    有什么必要(

--- src/main/java/dev/celestiacraft/cmi/common/block/test_multiblock/TestMultiblockBlock.java
+++ src/main/java/dev/celestiacraft/cmi/common/block/test_multiblock/TestMultiblockBlock.java
@@ -3,13 +3,11 @@ package dev.celestiacraft.cmi.common.block.test_multiblock;
 import dev.celestiacraft.cmi.api.register.multiblock.MultiblockControllerBlock;
 import dev.celestiacraft.cmi.common.register.CmiBlockEntityTypes;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
 public class TestMultiblockBlock extends MultiblockControllerBlock<TestMultiblockBlockEntity> {
 	public TestMultiblockBlock(Properties properties) {
-		super(Properties.copy(Blocks.STONE)
-				.sound(SoundType.STONE));
+		super(Properties.copy(Blocks.STONE));
 	}
 
 	@Override
