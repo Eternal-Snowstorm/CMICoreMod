31ce2815aaff2ab4fdc1c200bf35002169c9e9fe
Qi-Month <dy757410@163.com>
2026-03-18 17:54:55 +0800

    ModResource

--- src/main/java/dev/celestiacraft/cmi/utils/ModResource.java
+++ src/main/java/dev/celestiacraft/cmi/utils/ModResource.java
@@ -1,5 +1,6 @@
 package dev.celestiacraft.cmi.utils;
 
+import blusunrize.immersiveengineering.ImmersiveEngineering;
 import dev.celestiacraft.cmi.Cmi;
 import net.minecraft.resources.ResourceLocation;
 
@@ -8,9 +9,7 @@ public class ModResource {
 	public static final ResourceLocation SEA_WATER;
 
 	static {
-		TREATED_WOOD_STAIRS = ResourceLocation.parse(
-				"immersiveengineering:stairs_treated_wood_horizontal"
-		);
+		TREATED_WOOD_STAIRS = ImmersiveEngineering.rl("stairs_treated_wood_horizontal");
 		SEA_WATER = Cmi.loadResource("sea_water");
 	}
 }
