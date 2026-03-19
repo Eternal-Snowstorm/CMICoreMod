2944a5496c1451b777320f6fdae73af405f8e19d
Qi-Month <dy757410@163.com>
2026-03-19 21:21:20 +0800

    try?

--- src/main/java/dev/celestiacraft/cmi/common/block/test_coke_oven/TestCokeOvenBlock.java
+++ src/main/java/dev/celestiacraft/cmi/common/block/test_coke_oven/TestCokeOvenBlock.java
@@ -3,12 +3,29 @@ package dev.celestiacraft.cmi.common.block.test_coke_oven;
 import dev.celestiacraft.cmi.api.register.multiblock.ControllerBlock;
 import dev.celestiacraft.cmi.api.register.multiblock.ControllerBlockFacing;
 import dev.celestiacraft.cmi.common.register.CmiBlockEntityTypes;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class TestCokeOvenBlock extends ControllerBlock<TestCokeOvenBlockEntity> {
 	public TestCokeOvenBlock(Properties properties) {
 		super(Properties.copy(Blocks.STONE));
+	}
+
+	@Override
+	public <S extends BlockEntity> BlockEntityTicker<S> getTicker(Level level, BlockState state, BlockEntityType<S> type) {
+		if (level.isClientSide()) {
+			return null;
+		}
+
+		return (lvl, pos, st, be) -> {
+			if (be instanceof TestCokeOvenBlockEntity entity) {
+				TestCokeOvenBlockEntity.tick(lvl, pos, st, entity);
+			}
+		};
 	}
 
 	@Override
