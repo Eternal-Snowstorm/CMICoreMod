973a1a75f03100bfeeafe88818c94f8f6cf28857
Qi-Month <dy757410@163.com>
2026-03-22 02:18:32 +0800

    IEnergyItem

---
+++ src/main/java/dev/celestiacraft/cmi/common/item/SimpleBatteryItem.java
@@ -0,0 +1,63 @@
+package dev.celestiacraft.cmi.common.item;
+
+import dev.celestiacraft.cmi.api.register.item.energy.EnergyItemCapabilityProvider;
+import dev.celestiacraft.cmi.api.register.item.energy.IEnergyItem;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import org.jetbrains.annotations.NotNull;
+
+public class SimpleBatteryItem extends Item implements IEnergyItem {
+	public SimpleBatteryItem(Properties properties) {
+		super(properties.stacksTo(1));
+	}
+
+	@Override
+	public int getCapacity(ItemStack stack) {
+		return 150000;
+	}
+
+	@Override
+	public int getMaxReceive(ItemStack stack) {
+		return 1000;
+	}
+
+	@Override
+	public int getMaxExtract(ItemStack stack) {
+		return 1000;
+	}
+
+	@Override
+	public boolean isBarVisible(@NotNull ItemStack stack) {
+		return isEnergyBarVisible(stack);
+	}
+
+	@Override
+	public int getBarWidth(@NotNull ItemStack stack) {
+		return getEnergyBarWidth(stack);
+	}
+
+	@Override
+	public int getBarColor(@NotNull ItemStack stack) {
+		return getEnergyBarColor(stack);
+	}
+
+	@Override
+	public int getEnergyBarColor(ItemStack stack) {
+		float ratio = (float) getEnergyStored(stack) / getCapacity(stack);
+
+		if (ratio >= 0.6f) {
+			return 0x00FFFF;
+		} else if (ratio >= 0.2f) {
+			return 0xFFFF00;
+		} else {
+			return 0xFF0000;
+		}
+	}
+
+	@Override
+	public ICapabilityProvider initCapabilities(ItemStack stack, CompoundTag nbt) {
+		return new EnergyItemCapabilityProvider(stack, this);
+	}
+}
--- src/main/java/dev/celestiacraft/cmi/common/register/CmiItem.java
+++ src/main/java/dev/celestiacraft/cmi/common/register/CmiItem.java
@@ -4,17 +4,21 @@ import com.tterrag.registrate.util.entry.ItemEntry;
 import dev.celestiacraft.cmi.Cmi;
 import dev.celestiacraft.cmi.api.client.CmiTextures;
 import dev.celestiacraft.cmi.common.item.MysticPomeloItem;
+import dev.celestiacraft.cmi.common.item.SimpleBatteryItem;
 import dev.celestiacraft.cmi.common.item.TestBrushItem;
 
 public class CmiItem {
 	public static final ItemEntry<TestBrushItem> TEST_BRUSH;
 	public static final ItemEntry<MysticPomeloItem> MYSTIC_POMELO;
+	public static final ItemEntry<SimpleBatteryItem> SIMPLE_BATTERY;
 
 	static {
 		TEST_BRUSH = Cmi.REGISTRATE.item("test_brush", TestBrushItem::new)
 				.register();
 		MYSTIC_POMELO = Cmi.REGISTRATE.item("mystic_pomelo", MysticPomeloItem::new)
 				.model(CmiTextures.Items.setTexture("item/mystic_pomelo"))
+				.register();
+		SIMPLE_BATTERY = Cmi.REGISTRATE.item("simple_battery", SimpleBatteryItem::new)
 				.register();
 	}
 
