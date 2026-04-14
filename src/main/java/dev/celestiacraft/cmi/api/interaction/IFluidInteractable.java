221e405da2c59b74a450b87ea99c846e164aaac1
Qi-Month <dy757410@163.com>
2026-04-14 23:46:55 +0800

    加个强制性的东西

--- src/main/java/dev/celestiacraft/cmi/api/interaction/IFluidInteractable.java
+++ src/main/java/dev/celestiacraft/cmi/api/interaction/IFluidInteractable.java
@@ -5,10 +5,14 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.fluids.FluidUtil;
+import org.jetbrains.annotations.NotNull;
 
 public interface IFluidInteractable {
+	InteractionResult use(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult result);
+
 	default boolean useFluidInteraction() {
 		return false;
 	}
