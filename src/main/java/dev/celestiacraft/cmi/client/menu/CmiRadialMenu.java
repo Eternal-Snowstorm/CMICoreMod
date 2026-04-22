0e99c947ca0eed46363077b7721533986fc3afc6
Qi-Month <dy757410@163.com>
2026-04-22 13:31:58 +0800

    用不着斜杠

--- src/main/java/dev/celestiacraft/cmi/client/menu/CmiRadialMenu.java
+++ src/main/java/dev/celestiacraft/cmi/client/menu/CmiRadialMenu.java
@@ -4,8 +4,6 @@ import cc.sighs.auratip.api.action.Actions;
 import cc.sighs.auratip.api.radiamenu.RadialMenuBuilder;
 import cc.sighs.auratip.api.radiamenu.RadialMenuRegistry;
 import cc.sighs.auratip.data.RadialMenuData;
-import com.simibubi.create.AllItems;
-import com.simibubi.create.Create;
 import dev.celestiacraft.cmi.Cmi;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -30,7 +28,7 @@ public class CmiRadialMenu {
 				.slot(
 						"Claim Chunk",
 						ResourceLocation.parse("ftblibrary:textures/icons/map.png"),
-						Actions.runCommand("/ftbchunks claim"),
+						Actions.runCommand("ftbchunks claim"),
 						Component.translatable("radial.cmi.claim_chunk"),
 						"#77FFFFFF"
 				)
