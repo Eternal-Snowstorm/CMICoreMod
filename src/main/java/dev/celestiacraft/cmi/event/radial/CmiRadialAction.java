cb3b6bfa125f31f1c46ca9f35b4b731bf0281927
Qi-Month <dy757410@163.com>
2026-04-26 17:14:04 +0800

    先push

--- src/main/java/dev/celestiacraft/cmi/event/radial/CmiRadialAction.java
+++ src/main/java/dev/celestiacraft/cmi/event/radial/CmiRadialAction.java
@@ -6,9 +6,11 @@ import dev.celestiacraft.cmi.Cmi;
 import dev.celestiacraft.cmi.utils.ModResources;
 import mekanism.common.registries.MekanismItems;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
 public class CmiRadialAction {
@@ -42,25 +44,47 @@ public class CmiRadialAction {
 
 	private static void pickup(ItemStack stack) {
 		Minecraft minecraft = Minecraft.getInstance();
-		if (minecraft.player == null) {
+		Player player = minecraft.player;
+
+		if (player == null) {
 			return;
 		}
 
-		Inventory inventory = minecraft.player.getInventory();
+		if (!player.getItemInHand(InteractionHand.MAIN_HAND).isEmpty()) {
+			return;
+		}
 
-		int slot = inventory.findSlotMatchingItem(stack);
+		Inventory inventory = player.getInventory();
+
+		int slot = findItemIgnoringNBT(inventory, stack);
 		if (slot == -1) {
+			player.displayClientMessage(
+					Component.literal("NOT!"),
+					true
+			);
 			return;
 		}
 
-		ItemStack main = minecraft.player.getItemInHand(InteractionHand.MAIN_HAND);
-		if (!main.isEmpty()) {
-			return;
+		ItemStack stackInSlot = inventory.getItem(slot);
+		player.setItemInHand(InteractionHand.MAIN_HAND, stackInSlot);
+		inventory.setItem(slot, ItemStack.EMPTY);
+	}
+
+	/**
+	 * 忽略NBT查找物品
+	 *
+	 * @param inventory
+	 * @param item
+	 * @return
+	 */
+	private static int findItemIgnoringNBT(Inventory inventory, ItemStack item) {
+		for (int i = 0; i < inventory.getContainerSize(); i++) {
+			ItemStack stack = inventory.getItem(i);
+
+			if (!stack.isEmpty() && ItemStack.isSameItem(stack, item)) {
+				return i;
+			}
 		}
-
-		ItemStack wrench = inventory.getItem(slot);
-
-		minecraft.player.setItemInHand(InteractionHand.MAIN_HAND, wrench.copy());
-		inventory.removeItem(slot, wrench.getCount());
+		return -1;
 	}
 }
