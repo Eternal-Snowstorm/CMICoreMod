926fcbda5825bb188187ea743bb4b27826757836
Qi-Month <dy757410@163.com>
2026-04-26 14:17:25 +0800

    fix datagen

--- src/main/java/dev/celestiacraft/cmi/mixin/FluidStackJSMixin.java
+++
@@ -1,9 +0,0 @@
-package dev.celestiacraft.cmi.mixin;
-
-import dev.celestiacraft.cmi.compat.kubejs.utils.fluid.FluidStackJSUtils;
-import dev.latvian.mods.kubejs.fluid.FluidStackJS;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(FluidStackJS.class)
-public class FluidStackJSMixin implements FluidStackJSUtils {
-}
--- src/main/java/dev/celestiacraft/cmi/mixin/FluidWrapperMixin.java
+++
@@ -1,9 +0,0 @@
-package dev.celestiacraft.cmi.mixin;
-
-import dev.celestiacraft.cmi.compat.kubejs.utils.fluid.FluidWrapperUtils;
-import dev.latvian.mods.kubejs.fluid.FluidWrapper;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(FluidWrapper.class)
-public class FluidWrapperMixin implements FluidWrapperUtils {
-}
