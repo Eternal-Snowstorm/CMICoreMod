388a10b8dfc8c473bb0daf0a9ff8187365f7ba66
Qi-Month <dy757410@163.com>
2026-05-03 22:17:09 +0800

    接口

--- src/main/java/dev/celestiacraft/cmi/common/block/solar_boiler/SolarBoilerBlock.java
+++ src/main/java/dev/celestiacraft/cmi/common/block/solar_boiler/SolarBoilerBlock.java
@@ -4,7 +4,6 @@ import com.simibubi.create.foundation.block.IBE;
 import com.tterrag.registrate.providers.DataGenContext;
 import com.tterrag.registrate.providers.RegistrateBlockstateProvider;
 import com.tterrag.registrate.util.nullness.NonNullBiConsumer;
-import dev.celestiacraft.libs.api.interaction.IFluidInteractable;
 import dev.celestiacraft.libs.api.register.block.BasicBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
@@ -12,7 +11,7 @@ import net.minecraftforge.client.model.generators.BlockModelBuilder;
 import net.minecraftforge.client.model.generators.BlockModelProvider;
 import net.minecraftforge.client.model.generators.ConfiguredModel;
 
-public abstract class SolarBoilerBlock extends BasicBlock implements IBE<SolarBoilerBlockEntity>, IFluidInteractable {
+public abstract class SolarBoilerBlock extends BasicBlock implements IBE<SolarBoilerBlockEntity> {
 	public SolarBoilerBlock(Properties properties) {
 		super(properties.sound(SoundType.LANTERN));
 	}
