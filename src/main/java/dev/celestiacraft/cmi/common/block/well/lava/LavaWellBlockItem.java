613cdfc49458bc714c412fe0ed361637c6ac35ce
Qi-Month <dy757410@163.com>
2026-05-09 21:37:27 +0800

    fix

--- src/main/java/dev/celestiacraft/cmi/common/block/well/lava/LavaWellBlockItem.java
+++ src/main/java/dev/celestiacraft/cmi/common/block/well/lava/LavaWellBlockItem.java
@@ -1,11 +1,8 @@
 package dev.celestiacraft.cmi.common.block.well.lava;
 
-import dev.celestiacraft.libs.api.client.tooltip.TooltipContext;
+import dev.celestiacraft.libs.api.client.context.TooltipContext;
 import dev.celestiacraft.libs.api.register.multiblock.ControllerBlock;
 import dev.celestiacraft.libs.api.register.multiblock.ControllerBlockItem;
-import net.minecraft.network.chat.Component;
-
-import java.util.List;
 
 public class LavaWellBlockItem extends ControllerBlockItem {
 	public LavaWellBlockItem(ControllerBlock block, Properties properties) {
@@ -14,8 +11,7 @@ public class LavaWellBlockItem extends ControllerBlockItem {
 
 	@Override
 	public void addTooltips(TooltipContext context) {
-		List<Component> tooltip = context.getTooltip();
-		tooltip.add(Component.translatable("cmi.tooltip.lava_well.runningEnvironment"));
+		context.addTranslatable("cmi.tooltip.lava_well.runningEnvironment");
 		super.addTooltips(context);
 	}
 }
--- src/main/java/dev/celestiacraft/cmi/common/block/well/water/WaterWellBlockItem.java
+++ src/main/java/dev/celestiacraft/cmi/common/block/well/water/WaterWellBlockItem.java
@@ -1,11 +1,8 @@
 package dev.celestiacraft.cmi.common.block.well.water;
 
-import dev.celestiacraft.libs.api.client.tooltip.TooltipContext;
+import dev.celestiacraft.libs.api.client.context.TooltipContext;
 import dev.celestiacraft.libs.api.register.multiblock.ControllerBlock;
 import dev.celestiacraft.libs.api.register.multiblock.ControllerBlockItem;
-import net.minecraft.network.chat.Component;
-
-import java.util.List;
 
 public class WaterWellBlockItem extends ControllerBlockItem {
 	public WaterWellBlockItem(ControllerBlock block, Properties properties) {
@@ -14,8 +11,7 @@ public class WaterWellBlockItem extends ControllerBlockItem {
 
 	@Override
 	public void addTooltips(TooltipContext context) {
-		List<Component> tooltip = context.getTooltip();
-		tooltip.add(Component.translatable("cmi.tooltip.water_well.runningEnvironment"));
+		context.addTranslatable("cmi.tooltip.water_well.runningEnvironment");
 		super.addTooltips(context);
 	}
 }
