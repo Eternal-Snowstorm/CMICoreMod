0129e239b3bb070d0e5324c345bb9601a2e75a80
Qi-Month <dy757410@163.com>
2026-06-29 21:53:15 +0800

    mixin

---
+++ src/main/java/dev/celestiacraft/cmi/mixin/PackCompatibilityMixin.java
@@ -0,0 +1,14 @@
+package dev.celestiacraft.cmi.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.server.packs.repository.PackCompatibility;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(PackCompatibility.class)
+public class PackCompatibilityMixin {
+	@ModifyReturnValue(method = "isCompatible()Z",at = @At("RETURN"))
+	private boolean alwaysCompatible(boolean ori) {
+		return true;
+	}
+}
