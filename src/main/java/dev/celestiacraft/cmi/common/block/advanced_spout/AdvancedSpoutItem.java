c622dd3b1db36bcee4ec67c4fe954041c5246989
Qi-Month <dy757410@163.com>
2026-07-20 00:20:11 +0800

    AdvancedSpoutItem

--- src/main/java/dev/celestiacraft/cmi/common/block/advanced_spout/AdvancedSpoutItem.java
+++ src/main/java/dev/celestiacraft/cmi/common/block/advanced_spout/AdvancedSpoutItem.java
@@ -1,23 +1,73 @@
 package dev.celestiacraft.cmi.common.block.advanced_spout;
 
-import com.simibubi.create.content.processing.AssemblyOperatorBlockItem;
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.content.kinetics.belt.BeltBlock;
+import com.simibubi.create.content.kinetics.belt.BeltSlope;
+import com.simibubi.create.content.processing.AssemblyOperatorUseContext;
+import com.simibubi.create.content.processing.basin.BasinBlock;
+import dev.celestiacraft.libs.api.client.context.TooltipContext;
 import dev.celestiacraft.libs.api.register.block.BasicBlock;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
+import dev.celestiacraft.libs.api.register.block.BasicBlockItem;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
-import javax.annotation.Nullable;
-import java.util.List;
-
-public class AdvancedSpoutItem extends AssemblyOperatorBlockItem {
+public class AdvancedSpoutItem extends BasicBlockItem {
 	public AdvancedSpoutItem(BasicBlock block, Properties properties) {
 		super(block, properties);
 	}
 
 	@Override
-	public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> tooltip, @NotNull TooltipFlag flag) {
+	public void addTooltips(TooltipContext context) {
+		super.addTooltips(context);
+	}
 
+	@Override
+	public @NotNull InteractionResult place(BlockPlaceContext context) {
+		BlockPos placedOnPos = context.getClickedPos().relative(context.getClickedFace().getOpposite());
+		Level level = context.getLevel();
+		BlockState placedOnState = level.getBlockState(placedOnPos);
+
+		if (operatesOn(level, placedOnPos, placedOnState) && context.getClickedFace() == Direction.UP) {
+			if (level.getBlockState(placedOnPos.above(2)).canBeReplaced()) {
+				context = adjustContext(context, placedOnPos);
+			} else {
+				return InteractionResult.FAIL;
+			}
+		}
+
+		return super.place(context);
+	}
+
+	protected BlockPlaceContext adjustContext(BlockPlaceContext context, BlockPos placedOnPos) {
+		BlockPos up = placedOnPos.above(2);
+		return new AssemblyOperatorUseContext(
+				context.getLevel(),
+				context.getPlayer(),
+				context.getHand(),
+				context.getItemInHand(),
+				new BlockHitResult(
+						new Vec3(up.getX() + 0.5D + Direction.UP.getStepX() * 0.5D,
+								up.getY() + 0.5D + (double) Direction.UP.getStepY() * 0.5D,
+								up.getZ() + 0.5D + (double) Direction.UP.getStepZ() * 0.5D
+						),
+						Direction.UP,
+						up,
+						false
+				));
+	}
+
+	protected boolean operatesOn(LevelReader world, BlockPos pos, BlockState placedOnState) {
+		if (AllBlocks.BELT.has(placedOnState)) {
+			return placedOnState.getValue(BeltBlock.SLOPE) == BeltSlope.HORIZONTAL;
+		}
+		return BasinBlock.isBasin(world, pos) || AllBlocks.DEPOT.has(placedOnState) || AllBlocks.WEIGHTED_EJECTOR.has(placedOnState);
 	}
 }
