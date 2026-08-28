b8641a3c0d009a3beda6e56e6174ef00c05a8aea
Qi-Month <dy757410@163.com>
2026-08-29 00:51:51 +0800

    lombok太好用了

--- src/main/java/dev/celestiacraft/cmi/utils/metal/CmiMetal.java
+++ src/main/java/dev/celestiacraft/cmi/utils/metal/CmiMetal.java
@@ -1,5 +1,6 @@
 package dev.celestiacraft.cmi.utils.metal;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -7,23 +8,10 @@ import lombok.experimental.Accessors;
 @Getter
 @Setter
 @Accessors(chain = true)
+@AllArgsConstructor
 public class CmiMetal {
 	private final String id;
 	private int meltingPoint;
 	private String namespace;
 	private String byProduct;
-
-	public CmiMetal(String id) {
-		this.id = id;
-	}
-
-	public CmiMetal namespace(String namespace) {
-		this.namespace = namespace;
-		return this;
-	}
-
-	public CmiMetal byProduct(String value) {
-		byProduct = value;
-		return this;
-	}
 }
