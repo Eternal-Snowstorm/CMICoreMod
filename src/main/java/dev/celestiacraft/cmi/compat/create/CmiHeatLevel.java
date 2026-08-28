5432838e84e1cb585d577d4393a2b8e4a4d4bc53
Qi-Month <dy757410@163.com>
2026-08-28 21:49:31 +0800

    CmiHeatLevel

--- src/main/java/dev/celestiacraft/cmi/compat/create/CmiHeatLevel.java
+++ src/main/java/dev/celestiacraft/cmi/compat/create/CmiHeatLevel.java
@@ -1,20 +1,14 @@
 package dev.celestiacraft.cmi.compat.create;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum CmiHeatLevel {
 	GRILLED("grilled"),
 	HEATED("heated"),
 	SUPERHEATED("superheated");
 
 	private final String id;
-
-	CmiHeatLevel(String id) {
-		this.id = id;
-	}
-
-	public String toString() {
-		return id;
-	}
 }
